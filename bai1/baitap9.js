174e3b299b2daf68f7e9b69507dbda0e782efdcd
hieunv-0908 <nn650370@gmail.com>
2025-08-04 01:24:15 +0700

    sua lan 2

--- bai1/baitap9.js
+++ bai1/baitap9.js
@@ -1,16 +1,5 @@
 function mergeSortedArrays(arr1, arr2) {
-    let i = 0, j = 0;
-    const merged = [];
-
-    while (i < arr1.length && j < arr2.length) {
-        if (arr1[i] < arr2[j]) {
-            merged.push(arr1[i++]);
-        } else {
-            merged.push(arr2[j++]);
-        }
-    }
-
-    return [...merged, ...arr1.slice(i), ...arr2.slice(j)];
+    return [...merged, ...arr1, ...arr2].sort((a,b)=>a-b);
 }
 const a = [1, 2, 3, 5, 9];
 const b = [4, 6, 7, 8];
