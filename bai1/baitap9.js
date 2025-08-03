d94b1af49d0559e9af90b8cb29bc54cf49ec96a2
hieunv-0908 <nn650370@gmail.com>
2025-08-04 01:15:44 +0700

    thay doi bai 9

--- bai1/baitap9.js
+++ bai1/baitap9.js
@@ -1,5 +1,18 @@
-function sumArrays(...arrays) {
-    return arrays.map(arr => arr.reduce((sum, num) => sum + num, 0));
+function mergeSortedArrays(arr1, arr2) {
+    let i = 0, j = 0;
+    const merged = [];
+
+    while (i < arr1.length && j < arr2.length) {
+        if (arr1[i] < arr2[j]) {
+            merged.push(arr1[i++]);
+        } else {
+            merged.push(arr2[j++]);
+        }
+    }
+
+    return [...merged, ...arr1.slice(i), ...arr2.slice(j)];
 }
-const result = sumArrays([1, 2], [6, 7, 8], [12, 8]);
-console.log(result);
+const a = [1, 2, 3, 5, 9];
+const b = [4, 6, 7, 8];
+
+console.log(mergeSortedArrays(a, b));  
