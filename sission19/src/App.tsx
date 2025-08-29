40452d61521a8e11e7108f708170136e8b3c8a14
hieunv-0908 <nn650370@gmail.com>
2025-08-29 08:11:18 +0700

    ss19

---
+++ sission19/src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import ShopingCart from './components/ShopingCart'
+function App() {
+
+  return (
+    <>
+        <ShopingCart></ShopingCart>
+    </>
+  )
+}
+
+export default App
