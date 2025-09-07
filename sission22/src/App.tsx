072d5cb7d79d9db8867900fbe17861bce787c173
hieunv-0908 <nn650370@gmail.com>
2025-09-08 02:54:30 +0700

    ss22

---
+++ sission22/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ButtonComponent from './Components/ButtonComponent'
+// import InputComponent from './Components/InputComponent'
+import ComponentCart from './Components/ComponentCart'
+import DropdowComponent from './Components/DropdowComponent'
+import AlertComponent from './Components/AlertComponent'
+
+export default function App() {
+  return (
+    <div>
+      {/* <ButtonComponent /> */}
+      {/* <InputComponent /> */}
+      {/* <ComponentCart></ComponentCart> */}
+      {/* <DropdowComponent></DropdowComponent> */}
+      <AlertComponent></AlertComponent>
+    </div>
+  )
+}
